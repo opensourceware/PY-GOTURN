a87aa20cee1348a730a738a6859b8661e015dd96
Manpreet Kaur <manpreetkaur9411@gmail.com>
2017-12-17 12:42:57 -0500

    Regressor network fix AlexNet layers

--- goturn/network/regressor_network.py
+++ goturn/network/regressor_network.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -48,7 +50,6 @@ class Model(nn.Module):
 		self.conv4 = nn.Conv2d(384, 384, 3, padding=1, groups=2)
 		self.lrn4 = LRN(local_size=5, alpha=0.0001, beta=0.75)
 		self.conv5 = nn.Conv2d(384, 256, 3, padding=1, groups=2)
-		self.lrn5 = LRN(local_size=5, alpha=0.0001, beta=0.75)
 
 		self.conv1_p = nn.Conv2d(3, 96, 11, 4)
 		self.lrn1_p = LRN(local_size=5, alpha=0.0001, beta=0.75)
@@ -59,7 +60,6 @@ class Model(nn.Module):
 		self.conv4_p = nn.Conv2d(384, 384, 3, padding=1, groups=2)
 		self.lrn4_p = LRN(local_size=5, alpha=0.0001, beta=0.75)
 		self.conv5_p = nn.Conv2d(384, 256, 3, padding=1, groups=2)
-		self.lrn5_p = LRN(local_size=5, alpha=0.0001, beta=0.75)
 
 		self.fc6 = nn.Linear(18432, 4096)
 		self.fc7 = nn.Linear(4096, 4096)
@@ -69,32 +69,35 @@ class Model(nn.Module):
 		self.lstm = nn.LSTM(124, self.hidden_size, 1)
 		self.fc9 = nn.Linear(124, 4)
 
+		for param in self.parameters():
+			param.requires_grad = False
+		for param in self.lstm.parameters():
+			param.requires_grad = True
+		for param in self.fc9.parameters():
+			param.requires_grad = True
+
+
 	def forward(self, img, ref, hidden):
 		# seq_len = img.shape[0]
 		img_output = self.lrn1(F.max_pool2d(F.relu(self.conv1(img)), 3, 2))
-		# print(img_output.size())
 		img_output = self.lrn2(F.max_pool2d(F.relu(self.conv2(img_output)), 3, 2))
-		# print(img_output.size())
 		img_output = self.lrn3(F.relu(self.conv3(img_output)))
-		# print(img_output.size())
 		img_output = self.lrn4(F.relu(self.conv4(img_output)))
-		# print(img_output.size())
-		img_output = self.lrn5(F.relu(self.conv5(img_output)))
-		# print(img_output.size())
+		img_output = F.max_pool2d(F.relu(self.conv5(img_output)), 3, 2)
 
 		ref_output = self.lrn1(F.max_pool2d(F.relu(self.conv1(ref)), 3, 2))
 		ref_output = self.lrn2(F.max_pool2d(F.relu(self.conv2(ref_output)), 3, 2))
 		ref_output = self.lrn3(F.relu(self.conv3(ref_output)))
 		ref_output = self.lrn4(F.relu(self.conv4(ref_output)))
-		ref_output = self.lrn5(F.relu(self.conv5(ref_output)))
+		ref_output = F.max_pool2d(F.relu(self.conv5(ref_output)), 3, 2)
 
-		output = torch.cat(img_output.view(self.seq_len, 1, 18432/2), ref_output.view(self.seq_len, 1, 18432/2), axis=2)
+		output = torch.cat((img_output.view(self.seq_len, 9216), ref_output.view(self.seq_len, 9216)), 1)
 
-		output = nn.dropout(F.relu(self.fc6(output)))
-		output = nn.dropout(F.relu(self.fc7(output)))
-		output = nn.dropout(F.relu(self.fc7b(output)))
-		output = nn.dropout(F.relu(self.fc8(output)))
-		output, hidden = self.lstm(output, hidden)
+		output = F.dropout(F.relu(self.fc6(output)))
+		output = F.dropout(F.relu(self.fc7(output)))
+		output = F.dropout(F.relu(self.fc7b(output)))
+		output = F.dropout(F.relu(self.fc8(output)))
+		output, hidden = self.lstm(torch.unsqueeze(output, 1))
 		output = self.fc9(output)
 		return output
 
@@ -115,34 +118,36 @@ class Model(nn.Module):
 
 		for file in data_files:
 			inp, target, ground_truth = self.load_data(input_dir, file)
-			self.seq_len = inp.shape[0]
+			self.seq_len = inp.shape[0]-1
 			inp = Variable(torch.from_numpy(inp[1:]).type(torch.FloatTensor), requires_grad=False)
 			target = Variable(torch.from_numpy(target[:-1]).type(torch.FloatTensor), requires_grad=False)
-			ground_truth = Variable(torch.from_numpy(ground_truth).type(torch.FloatTensor), requires_grad=False)
+			ground_truth = Variable(torch.from_numpy(ground_truth[1:]).type(torch.FloatTensor), requires_grad=False)
 			hidden_0 = self.initHidden()
 			pred = self.forward(inp, target, hidden_0)
-			loss = self.loss(pred, ground_truth)
+			loss = self.loss(torch.squeeze(pred, 1), ground_truth)
 			if grad:
-				optimizer.zero_grad()
+				self.optimizer.zero_grad()
 				loss.backward()
-				optimizer.step()
+				self.optimizer.step()
 			else:
 				total_loss+=loss
-			print(file)
-			break
 		return total_loss
 
-	def train(self, input_dir, epochs=20):
-		self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+	def train(self, input_dir, epochs=50):
+		vars = []
+		for param in self.parameters():
+			if param.requires_grad == True:
+				vars.append(param)
+		self.optimizer = torch.optim.Adam(vars, lr=0.001)
 		prev_loss = np.inf
 		for iter in range(epochs):
 			self.fit(input_dir)
 			train_loss = self.fit(input_dir, False)
 			print('Loss epoch %i is %f' %(iter, train_loss))
-			if train_loss<prev_loss:
+			if float(train_loss.data)<prev_loss:
 				with open('tracker.pt', 'w') as f:
-					torch.save(model.state_dict(), f)
-			prev_loss = train_loss
+					torch.save(self.state_dict(), f)
+			prev_loss = float(train_loss.data)
 
 	def load_data(self, input_dir, file):
 		with open(input_dir+file+".search.pkl", "r") as f:
@@ -155,8 +160,8 @@ class Model(nn.Module):
 
 def main():
 	model = Model()
-	model.load_state_dict(torch.load("/home/manpk/Desktop/vision/project/PY-GOTURN/tracker.init.pt"))
-	input_dir = "/home/manpk/Desktop/vision/project/d/"
+	model.load_state_dict(torch.load("/home/manpreetkaur/FABMAP/tracking/PY-GOTURN/goturn/network/tracker.pt"))
+	input_dir = "/mnt/nfs/scratch1/manpreetkaur/tracking/data/"
 	model.train(input_dir)
 
 
